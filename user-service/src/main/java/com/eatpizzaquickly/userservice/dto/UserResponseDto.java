cc34cdb64c2e7f621b0e909b1facf719094fbe17
Woojin1123 <boom175839@gmail.com>
2024-11-22 08:28:05 +0900

    refactor: dto password 제거

--- user-service/src/main/java/com/eatpizzaquickly/userservice/dto/UserResponseDto.java
+++ user-service/src/main/java/com/eatpizzaquickly/userservice/dto/UserResponseDto.java
@@ -1,7 +1,7 @@
 package com.eatpizzaquickly.userservice.dto;
+
 import com.eatpizzaquickly.userservice.entity.User;
 import com.eatpizzaquickly.userservice.enums.UserRole;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,10 +13,6 @@ public class UserResponseDto {
     private String email;
     private String nickname;
     private UserRole userRole;
-
-    // password는 JSON 응답에서 제외되므로 제거할 수 있음
-    @JsonIgnore
-    private String password;
 
     public UserResponseDto(Long id,String email, String nickname, UserRole userRole) {
         this.id = id;
