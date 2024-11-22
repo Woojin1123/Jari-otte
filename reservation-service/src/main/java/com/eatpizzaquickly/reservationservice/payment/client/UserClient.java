c8f7d7d878da6ee363b3fb104cf6067d3f452691
Woojin1123 <boom175839@gmail.com>
2024-11-22 09:35:53 +0900

    refactor: feign 수정

--- reservation-service/src/main/java/com/eatpizzaquickly/reservationservice/payment/client/UserClient.java
+++ reservation-service/src/main/java/com/eatpizzaquickly/reservationservice/payment/client/UserClient.java
@@ -4,6 +4,7 @@ import com.eatpizzaquickly.reservationservice.common.config.FeignConfig;
 import com.eatpizzaquickly.reservationservice.payment.dto.response.UserResponseDto;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,12 @@ public interface UserClient {
     String CIRCUIT_BREAKER_NAME = "userService";
 
     @CircuitBreaker(name = CIRCUIT_BREAKER_NAME, fallbackMethod = "getUserByIdFallback")
-    @GetMapping(value = "/api/v1/users/{userId}", consumes = "application/json", produces = "application/json")
+    @GetMapping("/api/v1/users/{userId}")
     ResponseEntity<ApiResponse<UserResponseDto>> getUserById(@PathVariable("userId") Long userId);
 
     // Fallback method
-    default ApiResponse<UserResponseDto> getUserByIdFallback(Long userId, Exception ex) {
+    default ResponseEntity<ApiResponse<UserResponseDto>> getUserByIdFallback(Long userId, Exception ex) {
         // 사용자 정보 조회 실패 시
-        return ApiResponse.success("Failed to fetch user information", null);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.error("Failed to fetch user information", null));
     }
 }
